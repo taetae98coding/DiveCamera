291a7c6b908cf0587cb66484bc26988817ce7e96
taetae98coding <taetae98coding@gmail.com>
2026-05-26 21:21:06 +0900

    Split permission grant state test cases

--- core/permission/src/commonTest/kotlin/io/github/taetae98coding/divecamera/core/permission/RequiredPermissionGrantStateTest.kt
+++ core/permission/src/commonTest/kotlin/io/github/taetae98coding/divecamera/core/permission/RequiredPermissionGrantStateTest.kt
@@ -18,36 +18,50 @@ class RequiredPermissionGrantStateTest {
     }
 
     @Test
-    fun hasAllRequiredPermissionsReturnsFalseWhenAnyPermissionIsMissing() {
-        val states = listOf(
-            RequiredPermissionGrantState(
-                hasCamera = false,
-                hasMicrophone = true,
-                hasLocation = true,
-                hasPhotoSave = true,
-            ),
-            RequiredPermissionGrantState(
-                hasCamera = true,
-                hasMicrophone = false,
-                hasLocation = true,
-                hasPhotoSave = true,
-            ),
-            RequiredPermissionGrantState(
-                hasCamera = true,
-                hasMicrophone = true,
-                hasLocation = false,
-                hasPhotoSave = true,
-            ),
-            RequiredPermissionGrantState(
-                hasCamera = true,
-                hasMicrophone = true,
-                hasLocation = true,
-                hasPhotoSave = false,
-            ),
+    fun hasAllRequiredPermissionsReturnsFalseWhenCameraIsMissing() {
+        val state = RequiredPermissionGrantState(
+            hasCamera = false,
+            hasMicrophone = true,
+            hasLocation = true,
+            hasPhotoSave = true,
         )
 
-        states.forEach { state ->
-            assertFalse(state.hasAllRequiredPermissions)
-        }
+        assertFalse(state.hasAllRequiredPermissions)
+    }
+
+    @Test
+    fun hasAllRequiredPermissionsReturnsFalseWhenMicrophoneIsMissing() {
+        val state = RequiredPermissionGrantState(
+            hasCamera = true,
+            hasMicrophone = false,
+            hasLocation = true,
+            hasPhotoSave = true,
+        )
+
+        assertFalse(state.hasAllRequiredPermissions)
+    }
+
+    @Test
+    fun hasAllRequiredPermissionsReturnsFalseWhenLocationIsMissing() {
+        val state = RequiredPermissionGrantState(
+            hasCamera = true,
+            hasMicrophone = true,
+            hasLocation = false,
+            hasPhotoSave = true,
+        )
+
+        assertFalse(state.hasAllRequiredPermissions)
+    }
+
+    @Test
+    fun hasAllRequiredPermissionsReturnsFalseWhenPhotoSaveIsMissing() {
+        val state = RequiredPermissionGrantState(
+            hasCamera = true,
+            hasMicrophone = true,
+            hasLocation = true,
+            hasPhotoSave = false,
+        )
+
+        assertFalse(state.hasAllRequiredPermissions)
     }
 }
